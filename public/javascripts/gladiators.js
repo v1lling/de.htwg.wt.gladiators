9d902d7c61079b4cd4b3d7194106a7476d31805a
Sascha Villing <sascha.villing@web.de>
2020-11-12 11:20:57 +0100

    Add sendRequest to gladiators.js

--- public/javascripts/gladiators.js
+++ public/javascripts/gladiators.js
@@ -1,18 +1,41 @@
+/**
+* Ends turn and sets new current player
+ */
+function onClickEndTurn() {
+    var fnSuccess = function(result) {
+        console.log(result);
+        // set active player
+    }
+    sendRequest("/gladiators/end", fnSuccess);
+}
 
+/**
+ * Loads the controller as json
+ * Initializes the game
+ */
 function loadJson() {
+    var fnSuccess = function(result) {
+        console.log(result);
+        // initializing
+    }
+    sendRequest("/json", fnSuccess);
+}
+
+/**
+ * Sends a request to the server
+ * @param {string} path - The request URL
+ * @param {function} fnSuccess - Callback function for success
+ */
+function sendRequest(path, fnSuccess) {
     $.ajax({
         method: "GET",
-        url: "/json",
+        url: path,
         dataType: "json",
-
         success: function (result) {
+            fnSuccess(result);
+        },
+        error: function(result) {
             console.log(result);
-            /*
-            grid = new Grid(result.grid.size);
-            grid.fill(result.grid.cells);
-            updateGrid(grid);
-            registerClickListener();
-            */
         }
     });
 }
