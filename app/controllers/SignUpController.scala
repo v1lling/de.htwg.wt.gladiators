b37e5d8b5e8948f7e028cf9fa79952bfb52acc74
Sascha Villing <sascha.villing@web.de>
2021-01-17 09:41:10 +0100

    Deactivate email

--- app/controllers/SignUpController.scala
+++ app/controllers/SignUpController.scala
@@ -45,6 +45,7 @@ class SignUpController @Inject() (
         userService.retrieve(loginInfo).flatMap {
           case Some(user) =>
             val url = Calls.signin.absoluteURL()
+            /*
             mailerClient.send(Email(
               subject = Messages("email.already.signed.up.subject"),
               from = Messages("email.from"),
@@ -52,6 +53,7 @@ class SignUpController @Inject() (
               bodyText = Some(views.txt.emails.alreadySignedUp(user, url).body),
               bodyHtml = Some(views.html.emails.alreadySignedUp(user, url).body)
             ))
+            */
 
             Future.successful(result)
           case None =>
@@ -74,6 +76,7 @@ class SignUpController @Inject() (
               authToken <- authTokenService.create(user.userID)
             } yield {
               val url = routes.ActivateAccountController.activate(authToken.id).absoluteURL()
+              /*
               mailerClient.send(Email(
                 subject = Messages("email.sign.up.subject"),
                 from = Messages("email.from"),
@@ -81,6 +84,7 @@ class SignUpController @Inject() (
                 bodyText = Some(views.txt.emails.signUp(user, url).body),
                 bodyHtml = Some(views.html.emails.signUp(user, url).body)
               ))
+              */
 
               eventBus.publish(SignUpEvent(user, request))
               result
