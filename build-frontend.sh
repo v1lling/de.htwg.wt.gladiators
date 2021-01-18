bcecb7843ff9787d4f4978f80bd9d040f6367f3d
v1lling <sascha.villing@web.de>
2021-01-18 16:50:07 +0100

    Automatically built frontend

--- build-frontend.sh
+++ build-frontend.sh
@@ -5,12 +5,10 @@ echo "Frontend successfully built"
 sleep 3
 rm -rf ../de.htwg.wt.gladiators/public/frontend
 echo "Removed old frontend"
-sleep 3
 mkdir ../de.htwg.wt.gladiators/public/frontend
 cd ../de.htwg.wt.gladiators
 cp -r ../de.htwg.wt.gladiators-vue/dist/* ./public/frontend
 echo "Moved new frontend"
-sleep 3
 git add .
 if [ -z "$1" ]
 then
